f00fd70eaee09ec544f83dadc9c072dc35f2a34f
Ibrahim jaber <jber595@gmail.com>
2021-04-30 14:14:37 -0300

    UP
    UP

---
+++ ThirdPartyAPI-NET-Core/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ThirdPartyAPI_NET_Core.Models;
+using ThirdPartyAPI_NET_Core.NASAClient;
+
+namespace ThirdPartyAPI_NET_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly NASAPicAPIClient _client;
+
+        public HomeController
+            (ILogger<HomeController> logger, NASAPicAPIClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var response = await _client.Get();
+                return View(response);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ThirdPartyAPI-NET-Core/NASAClient/NASAPicAPIClient.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ThirdPartyAPI_NET_Core.Models;
+
+namespace ThirdPartyAPI_NET_Core.NASAClient
+{
+    public class NASAPicAPIClient : HttpClient
+    {
+        private readonly string BasePath;
+
+        private readonly string MEDIA_TYPE;
+
+        private readonly string APIKEY;
+
+        public NASAPicAPIClient
+            (string baseAddress, string basePath,string mediaType,string apiKey)
+        {
+            BaseAddress = new Uri(baseAddress);
+            BasePath = basePath;
+            MEDIA_TYPE = mediaType;
+            APIKEY = apiKey;
+        }
+
+        public async Task<PicOfTheDay> Get()
+        {
+            try
+            {
+                SetupHeaders();
+        
+                var response = await GetAsync(BasePath + $"?api_key={APIKEY}");
+        
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+        
+                    var returnModel = JsonConvert.DeserializeObject
+                        <PicOfTheDay>(result);
+        
+                    return returnModel;
+                }
+                else
+                {
+                    throw new Exception
+                        ($"Failed to retrieve items returned {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        protected virtual void SetupHeaders()
+        {
+            DefaultRequestHeaders.Clear();
+
+            //Define request data format  
+            DefaultRequestHeaders.Accept.Add
+                (new MediaTypeWithQualityHeaderValue
+                (MEDIA_TYPE));
+        }
+
+    }
+}
